209b7baa8a706df09a45090ec2a05bb2035e02a4
Swapnadip Paul <159642690+SwapnadipPaul@users.noreply.github.com>
2025-10-16 18:13:37 +0530

    Frontend updates by Paul

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import 'flowbite'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+// Flowbite CSS (ensure UI components have styles)
+import "flowbite/dist/flowbite.css";
+import "flowbite";
+
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
